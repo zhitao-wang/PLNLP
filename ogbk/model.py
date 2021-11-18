3d3d7d691dcebc6952a99883eb176bd39288380d
zhitaowang <zhitaowang@tencent.com>
2021-11-18 17:08:33 +0800

    test

--- ogbk/model.py
+++ ogbk/model.py
@@ -164,6 +164,7 @@ class Model(object):
             pos_edge = pos_train_edge[perm].t()
             neg_edge = global_perm_neg_sample(data.edge_index, self.num_nodes,
                            pos_edge.size(1), self.num_neg)
+            neg_edge = torch.reshape(neg_edge, (-1, 2)).t()
             # neg_edge = torch.reshape(neg_train_edge[perm], (-1, 2)).t()
             pos_out = self.predictor(h[pos_edge[0]], h[pos_edge[1]])
             neg_out = self.predictor(h[neg_edge[0]], h[neg_edge[1]])
