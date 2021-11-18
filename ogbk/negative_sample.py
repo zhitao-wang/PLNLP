70f1dcb9a507c89fc37a99a21c586eb85bd920d6
zhitaowang <zhitaowang@tencent.com>
2021-11-18 17:17:53 +0800

    Revert test
    This reverts commit bc74a62d

--- ogbk/negative_sample.py
+++ ogbk/negative_sample.py
@@ -52,15 +52,12 @@ def local_random_neg_sample(pos_edges, num_nodes, num_neg):
         (neg_src, neg_dst), dim=-1), (-1, num_neg, 2))
 
 
-def local_neg_sample(pos_edges, num_nodes, num_neg, device=None):
+def local_neg_sample(pos_edges, num_nodes, num_neg):
     neg_src = pos_edges[:, 0]
     neg_src = torch.reshape(neg_src, (-1, 1)).repeat(1, num_neg)
     neg_src = torch.reshape(neg_src, (-1,))
     neg_dst = torch.randint(
         0, num_nodes, (num_neg * pos_edges.size(0),), dtype=torch.long)
-    if device is not None:
-        neg_dst = neg_dst.to(device)
-        neg_src = neg_src.to(device)
     return torch.reshape(torch.stack(
         (neg_src, neg_dst), dim=-1), (-1, num_neg, 2))
 
