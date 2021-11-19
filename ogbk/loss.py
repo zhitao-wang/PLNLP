437bcb9e56ff7ec3f5387538b861cfd4b6a3c586
zhitaowang <zhitaowang@tencent.com>
2021-11-19 17:36:14 +0800

    loss update

--- ogbk/loss.py
+++ ogbk/loss.py
@@ -5,13 +5,13 @@ import torch
 def auc_loss(pos_out, neg_out, num_neg):
     pos_out = torch.reshape(pos_out, (-1, 1))
     neg_out = torch.reshape(neg_out, (-1, num_neg))
-    return torch.square(1 - (pos_out - neg_out)).mean()
+    return torch.square(1 - (pos_out - neg_out)).sum()
 
 
 def adaptive_auc_loss(margin, pos_out, neg_out, num_neg):
     pos_out = torch.reshape(pos_out, (-1, 1))
     neg_out = torch.reshape(neg_out, (-1, num_neg))
-    return torch.square(margin - (pos_out - neg_out)).mean()
+    return torch.square(margin - (pos_out - neg_out)).sum()
 
 
 def log_rank_loss(pos_out, neg_out, num_neg):
